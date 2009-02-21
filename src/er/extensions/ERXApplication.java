f8ce1eb4ea67e924f51dfd3a4756a720e46999f2
s-edwards <edwards@cs.vt.edu>
2009-02-21 21:54:32 +0000

    Changes for latest Wonder version and latest OGNL version.

---
+++ src/er/extensions/ERXApplication.java
@@ -0,0 +1,23 @@
+package er.extensions;
+
+/**
+ * This stub is provided for backwards compatibility with the old
+ * WO 5.2 version of WOJSPServlet_client.jar.  It provides access
+ * to inherited public static methods, but prevents use as a parent
+ * class.
+ *
+ * @deprecated Use er.extensions.appserver.ERXApplication instead.
+ * @author edwards
+ * @version $Id$
+ */
+public final class ERXApplication
+	extends er.extensions.appserver.ERXApplication
+{
+	/**
+	 * No one should ever create an instance of this stub.
+	 */
+	private ERXApplication()
+	{
+		/* empty */
+	}
+}
